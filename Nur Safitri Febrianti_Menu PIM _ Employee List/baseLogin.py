f59e66c4944bfb0a86b2eea0643137bc461824ea
Nur Safitri Febrianti <safitrifebrian93@gmail.com>
2023-02-25 22:43:28 +0700

    First Commit

---
+++ Nur Safitri Febrianti_Menu PIM _ Employee List/baseLogin.py
@@ -0,0 +1,19 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from page import element
+from data import inputan
+
+def valid_login(driver):
+        url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        driver.get(url)
+        driver.maximize_window()
+        time.sleep(1)
+        driver.find_element(By.NAME, element.Username).send_keys(inputan.username) #isi username
+        time.sleep(1)
+        driver.find_element(By.NAME, element.password).send_keys(inputan.password) #isi password
+        time.sleep(1)    
+        driver.find_element(By.CLASS_NAME, element.login_btn).click() #klik login
+        time.sleep(1)
---
+++ Nur Safitri Febrianti_Menu PIM _ Employee List/data.py
@@ -0,0 +1,6 @@
+class inputan() :
+    username = "Admin"
+    password = "admin123"
+    url_valid_login = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    header_dashboard = "Dashboard"
+    employee_list = "Employee List"
